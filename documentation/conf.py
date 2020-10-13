a8ca31ba539576adfc950a9d31254f9337b05e13
Melissa Lajtos <54413386+melissa05@users.noreply.github.com>
2020-10-13 22:46:18 +0200

    Added requirements regarding documentation.

--- documentation/conf.py
+++ documentation/conf.py
@@ -31,6 +31,7 @@ release = '1.0.dev0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# Unfortunately, paramlinks does not work like it should...
 extensions = ['sphinx.ext.autodoc', 'sphinx_paramlinks']
 
 # Add any paths that contain templates here, relative to this directory.
