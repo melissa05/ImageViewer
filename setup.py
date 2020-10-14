d16a7510b1a295b3a91365af4cd8d7b259c7d351
Melissa Lajtos <54413386+melissa05@users.noreply.github.com>
2020-10-14 21:48:56 +0200

    Still trying to get things right for readthedocs.

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
         author_email='m.lajtos@student.tugraz.at',
         description='Medical image viewer which works with h5 and dicom data formats.',
         python_requires='>=3.6',
-        install_requires=['PyQt5>=5.14', 'matplotlib>=3.2', 'h5py>=2.10', 'numpy>=1.18', 'pydicom>1.4']
+        install_requires=['PyQt5>=5.14', 'PyQt5.QtCore.pyqtSlot', 'matplotlib>=3.2', 'h5py>=2.10', 'numpy>=1.18', 'pydicom>1.4']
 )
