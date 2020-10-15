69a184e2c93fa5fc7b75620b612b4610ebade1a9
Melissa Lajtos <54413386+melissa05@users.noreply.github.com>
2020-10-15 19:25:44 +0200

    Still trying to get things right for readthedocs.

--- documentation/conf.py
+++ documentation/conf.py
@@ -34,7 +34,7 @@ release = '1.0.dev0'
 extensions = ['sphinx.ext.autodoc', 'sphinx_paramlinks']
 
 autodoc_mock_imports = ['sip', 'PyQt5', 'PyQt5.QtGui', 'PyQt5.QtCore', 'PyQt5.QtWidgets', 'PyQt5.QtWidgets.QWidget',
-                        'PyQt5.QtCore.pyqtSlot', 'QtWidgets.QMainWindow', 'PyQt5.QtCore.QRunnable',
+                        'PyQt5.QtCore.pyqtSlot', 'PyQt5.QtCore.pyqtSignal' 'QtWidgets.QMainWindow', 'PyQt5.QtCore.QRunnable',
                         'PyQt5.QtCore.QObject', 'matplotlib', 'h5py', 'pydicom', 'numpy']
 
 # Add any paths that contain templates here, relative to this directory.
